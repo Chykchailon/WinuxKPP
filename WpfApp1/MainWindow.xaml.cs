fc4a5a7c412927dc1dbe886ce5fc3e13e951c8c6
user2 <user2@CLASS3105>
2023-03-29 17:38:43 +0700

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        ApplicationContext db;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            db = new ApplicationContext();
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = tbLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+            string confirm = confBox.Password.Trim();
+            string email = tbMail.Text.Trim().ToLower();
+
+            if (login.Length < 5)
+            {
+                tbLogin.ToolTip = "Некорректный ввод";
+                tbLogin.Background = Brushes.Red;
+            } else if (pass.Length < 8)
+            {
+                passBox.ToolTip = "Некорректный ввод";
+                passBox.Background = Brushes.Red;
+            } else if (pass != confirm)
+            {
+                confBox.ToolTip = "Некорректный ввод";
+                confBox.Background = Brushes.Red;
+            } else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                tbMail.ToolTip = "Некорректный ввод";
+                tbMail.Background = Brushes.Red;
+            }
+            else
+            {
+                tbLogin.ToolTip = "";
+                tbLogin.Background = Brushes.LightGreen;
+
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.LightGreen;
+                
+                confBox.ToolTip = "";
+                confBox.Background = Brushes.LightGreen;
+                
+                tbMail.ToolTip = "";
+                tbMail.Background = Brushes.LightGreen;
+
+                MessageBox.Show("Данные приняты С==3");
+            }
+        }
+
+        //User user = new User(login, email, pass);
+        //db.Users.Add(user);
+        //db.SaveChanges();
+    }
+}
